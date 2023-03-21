86b103113a8816cf23e5f695929ef5d1213f6bad
Avijit Biswas <avijit@devdynamics.ai>
2023-03-21 21:09:56 +0530

    added firebase firestore to add salescode and api fetch

--- src/views/auth/signUp/index.jsx
+++ src/views/auth/signUp/index.jsx
@@ -21,8 +21,8 @@
 
 */
 
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 // Chakra imports
 import {
   Box,
@@ -38,61 +38,63 @@ import {
   InputRightElement,
   Text,
   useColorModeValue,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 // Custom components
-import { HSeparator } from 'components/separator/Separator';
-import DefaultAuth from 'layouts/auth/Default';
+import { HSeparator } from "components/separator/Separator";
+import DefaultAuth from "layouts/auth/Default";
 // Assets
-import illustration from 'assets/img/auth/auth.png';
-import { FcGoogle } from 'react-icons/fc';
-import { MdOutlineRemoveRedEye } from 'react-icons/md';
-import { RiEyeCloseLine } from 'react-icons/ri';
+import illustration from "assets/img/auth/auth.png";
+import { FcGoogle } from "react-icons/fc";
+import { MdOutlineRemoveRedEye } from "react-icons/md";
+import { RiEyeCloseLine } from "react-icons/ri";
 // Contexts
-import { AuthContext } from 'contexts/AuthContext';
+import { AuthContext } from "contexts/AuthContext";
 
 function SignUp() {
   // Chakra color mode
-  const textColor = useColorModeValue('navy.700', 'white');
-  const textColorSecondary = 'gray.400';
-  const textColorDetails = useColorModeValue('navy.700', 'secondaryGray.600');
-  const textColorBrand = useColorModeValue('brand.500', 'white');
-  const textColorError = 'pink.600';
-  const brandStars = useColorModeValue('brand.500', 'brand.400');
-  const googleBg = useColorModeValue('secondaryGray.300', 'whiteAlpha.200');
-  const googleText = useColorModeValue('navy.700', 'white');
+  const textColor = useColorModeValue("navy.700", "white");
+  const textColorSecondary = "gray.400";
+  const textColorDetails = useColorModeValue("navy.700", "secondaryGray.600");
+  const textColorBrand = useColorModeValue("brand.500", "white");
+  const textColorError = "pink.600";
+  const brandStars = useColorModeValue("brand.500", "brand.400");
+  const googleBg = useColorModeValue("secondaryGray.300", "whiteAlpha.200");
+  const googleText = useColorModeValue("navy.700", "white");
   const googleHover = useColorModeValue(
-    { bg: 'gray.200' },
-    { bg: 'whiteAlpha.300' }
+    { bg: "gray.200" },
+    { bg: "whiteAlpha.300" }
   );
   const googleActive = useColorModeValue(
-    { bg: 'secondaryGray.300' },
-    { bg: 'whiteAlpha.200' }
+    { bg: "secondaryGray.300" },
+    { bg: "whiteAlpha.200" }
   );
   // states and functions
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [loginError, setLoginError] = React.useState('');
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [loginError, setLoginError] = React.useState("");
+  const [salesCode, setSalesCode] = React.useState("");
   const [show, setShow] = React.useState(false);
   const { signUp } = React.useContext(AuthContext);
   const handleClick = () => setShow(!show);
   const handleSignUp = async () => {
-    setLoginError('');
-    const error = await signUp(email, password);
+    setLoginError("");
+    const error = await signUp(email, password, salesCode);
     if (error) setLoginError(error);
   };
+
   return (
     <DefaultAuth illustrationBackground={illustration} image={illustration}>
       <Flex
-        maxW={{ base: '100%', md: 'max-content' }}
+        maxW={{ base: "100%", md: "max-content" }}
         w="100%"
-        mx={{ base: 'auto', lg: '0px' }}
+        mx={{ base: "auto", lg: "0px" }}
         me="auto"
         h="100%"
         alignItems="start"
         justifyContent="center"
-        mb={{ base: '30px', md: '60px' }}
-        px={{ base: '25px', md: '0px' }}
-        mt={{ base: '40px', md: '14vh' }}
+        mb={{ base: "30px", md: "60px" }}
+        px={{ base: "25px", md: "0px" }}
+        mt={{ base: "40px", md: "14vh" }}
         flexDirection="column"
       >
         <Box me="auto">
@@ -112,13 +114,13 @@ function SignUp() {
         <Flex
           zIndex="2"
           direction="column"
-          w={{ base: '100%', md: '420px' }}
+          w={{ base: "100%", md: "420px" }}
           maxW="100%"
           background="transparent"
           borderRadius="15px"
-          mx={{ base: 'auto', lg: 'unset' }}
+          mx={{ base: "auto", lg: "unset" }}
           me="auto"
-          mb={{ base: '20px', md: 'auto' }}
+          mb={{ base: "20px", md: "auto" }}
         >
           <FormControl>
             <FormLabel
@@ -135,7 +137,7 @@ function SignUp() {
               isRequired={true}
               variant="auth"
               fontSize="sm"
-              ms={{ base: '0px', md: '0px' }}
+              ms={{ base: "0px", md: "0px" }}
               type="email"
               placeholder="mail@simmmple.com"
               mb="24px"
@@ -153,6 +155,7 @@ function SignUp() {
             >
               Mot de passe<Text color={brandStars}>*</Text>
             </FormLabel>
+
             <InputGroup size="md">
               <Input
                 isRequired={true}
@@ -160,7 +163,7 @@ function SignUp() {
                 placeholder="Min. 8 characters"
                 mb="24px"
                 size="lg"
-                type={show ? 'text' : 'password'}
+                type={show ? "text" : "password"}
                 variant="auth"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -168,12 +171,24 @@ function SignUp() {
               <InputRightElement display="flex" alignItems="center" mt="4px">
                 <Icon
                   color={textColorSecondary}
-                  _hover={{ cursor: 'pointer' }}
+                  _hover={{ cursor: "pointer" }}
                   as={show ? RiEyeCloseLine : MdOutlineRemoveRedEye}
                   onClick={handleClick}
                 />
               </InputRightElement>
             </InputGroup>
+
+            <Input
+              isRequired={true}
+              fontSize="sm"
+              placeholder="Enter Sales code"
+              mb="24px"
+              size="lg"
+              type="text"
+              variant="auth"
+              value={salesCode}
+              onChange={(e) => setSalesCode(e.target.value)}
+            />
             <Button
               fontSize="sm"
               variant="brand"
