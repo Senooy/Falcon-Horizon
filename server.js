1c503ecc50d78ef52ad41bfb084769a32a2cd9c8
Dokgo <nskhelifi@gmail.com>
2023-04-11 12:37:41 +0200

    API manager access

--- server.js
+++ server.js
@@ -23,14 +23,28 @@ app.get("/api/salesforce_data", async (req, res) => {
       },
     });
 
-    const salesDataUrl = `https://circet.my.salesforce.com/services/data/v56.0/sobjects/Contact/${req.query.salesCode}/Sales__r`;
-    const { data: salesData } = await axios.get(salesDataUrl, {
-      headers: {
-        Authorization: `Bearer ${data.access_token}`,
-      },
-    });
+    const fetchSalesData = async (salesCode) => {
+      const salesDataUrl = `https://circet.my.salesforce.com/services/data/v56.0/sobjects/Contact/${salesCode}/Sales__r`;
+      const { data: salesData } = await axios.get(salesDataUrl, {
+        headers: {
+          Authorization: `Bearer ${data.access_token}`,
+        },
+      });
+      return salesData;
+    };
 
-    res.json(salesData);
+    let salesCodes = req.query.salesCode;
+    if (!Array.isArray(salesCodes)) {
+      salesCodes = [salesCodes];
+    }
+
+    const allSalesData = [];
+    for (const salesCode of salesCodes) {
+      const salesData = await fetchSalesData(salesCode);
+      allSalesData.push(...salesData);
+    }
+
+    res.json(allSalesData);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Une erreur est survenue lors de la récupération des données." });
