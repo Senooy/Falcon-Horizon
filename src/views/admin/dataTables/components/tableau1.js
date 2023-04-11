be09bff3c934552a97406fc9a88eb7a047221d7a
Dokgo <nskhelifi@gmail.com>
2023-04-11 15:01:52 +0200

    margin pagination + dark mode

--- src/views/admin/dataTables/components/tableau1.js
+++ src/views/admin/dataTables/components/tableau1.js
@@ -314,17 +314,19 @@ const Tableau = () => {
       ))}
   </Tbody>
 </Table>
-<ReactPaginate
-  previousLabel={"←"}
-  nextLabel={"→"}
-  pageCount={pageCount}
-  onPageChange={handlePageClick}
-  containerClassName={"pagination"}
-  previousLinkClassName={"pagination__link"}
-  nextLinkClassName={"pagination__link"}
-  disabledClassName={"pagination__link--disabled"}
-  activeClassName={"pagination__link--active"}
-/>
+<Box mt={6}>
+  <ReactPaginate
+    previousLabel={"←"}
+    nextLabel={"→"}
+    pageCount={pageCount}
+    onPageChange={handlePageClick}
+    containerClassName={"pagination"}
+    previousLinkClassName={"pagination__link"}
+    nextLinkClassName={"pagination__link"}
+    disabledClassName={"pagination__link--disabled"}
+    activeClassName={"pagination__link--active"}
+  />
+</Box>
 </Box>
 );
 };
