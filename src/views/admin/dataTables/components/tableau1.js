e850103151a16214c54c6fe8ca8930ad089d03d4
Dokgo <nskhelifi@gmail.com>
2023-06-06 18:52:33 +0200

    fix

--- src/views/admin/dataTables/components/tableau1.js
+++ src/views/admin/dataTables/components/tableau1.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
   Box,
   useColorMode,
@@ -15,91 +15,37 @@ import {
   ButtonGroup,
   Button,
   Flex,
-} from '@chakra-ui/react';
-import axios from 'axios';
-import { AuthContext } from 'contexts/AuthContext';
-import ReactPaginate from 'react-paginate';
-import './pagination.css';
-import { FaAngleDown } from 'react-icons/fa';
-import { MdBarChart } from 'react-icons/md';
-import { Link } from 'react-router-dom';
-import { Input } from '@chakra-ui/react';
-import { Select } from '@chakra-ui/react';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import './i18n.js'
-import { useTranslation } from 'react-i18next';
+} from "@chakra-ui/react";
+import axios from "axios";
+import { AuthContext } from "contexts/AuthContext";
+import ReactPaginate from "react-paginate";
+import "./pagination.css";
+import { FaAngleDown } from "react-icons/fa";
+import { MdBarChart } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const PER_PAGE = 100;
 
 const Tableau = () => {
   const { colorMode } = useColorMode();
-  const { t } = useTranslation(); // Utilisation de la fonction t() pour traduire les termes
   const { user } = React.useContext(AuthContext);
   const shouldHideTable = user && user.profileData && user.profileData.admin;
 
-  const periods = ['Semaine', 'Mois', 'Année'];
+  const periods = ["Semaine", "Mois", "Année"];
   const [records, setRecords] = useState([]);
   const [filteredRecords, setFilteredRecords] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
   const [sortConfig, setSortConfig] = useState({
-    key: 'CreatedDate',
+    key: "CreatedDate",
     ascending: false,
   });
   const [collapsedRowId, setCollapsedRowId] = useState(null);
-  const [filter, setFilter] = useState({ period: 'Tous', status: 'Tous' });
+  const [filter, setFilter] = useState({ period: "Tous", status: "Tous" });
   const getRowColor = (status) => {
     const colors = getRowColors(status);
-    return colorMode === 'light' ? colors.light : colors.dark;
-  };
-
-  const [searchValue, setSearchValue] = useState('');  // NEW STATE FOR SEARCH VALUE
-
-  const [selectedMonth, setSelectedMonth] = useState('');
-
-  const [selectedDate, setSelectedDate] = useState(null);
-
-
-
-
-
-  const handleSearchChange = (e) => {
-    setSearchValue(e.target.value);
-  };
-
-  const handleMonthChange = (event) => {
-    setSelectedMonth(event.target.value);
-    const filtered = filterRecords(filter.period, filter.status, filter.hasConnectingDate);
-    setFilteredRecords(filtered);
+    return colorMode === "light" ? colors.light : colors.dark;
   };
   
-  const handleDateChange = (date) => {
-    setSelectedDate(date);  // Définir la date sélectionnée
-    const filtered = filterRecords(filter.period, filter.status, filter.hasConnectingDate);
-    setFilteredRecords(filtered);
-  };
-  
-
-  const searchRecords = (records) => {
-    return records.filter(record => {
-      const { TchProspectName__c, ProspectMobilePhone__c, OrderNumber__c } = record;
-      const searchLower = searchValue.toLowerCase();
-      return (TchProspectName__c && TchProspectName__c.toLowerCase().includes(searchLower)) ||
-             (ProspectMobilePhone__c && ProspectMobilePhone__c.toLowerCase().includes(searchLower)) ||
-             (OrderNumber__c && OrderNumber__c.toLowerCase().includes(searchLower));
-    });
-  };
-
-  useEffect(() => {
-    const filtered = filterRecords(filter.period, filter.status);
-    const searched = searchRecords(filtered);
-    setFilteredRecords(searched);
-  }, [records, filter, searchValue, selectedMonth, selectedDate]);
-
-  
-  const now = new Date();
-
-  const oneDay = 24 * 60 * 60 * 1000;
 
   const fetchData = async () => {
     try {
@@ -108,37 +54,24 @@ const Tableau = () => {
           `${user.profileData.salesCode}`
       );
       setRecords(data.records);
-      const filtered = filterRecords(filter.period, filter.status, filter.hasConnectingDate);
-      setFilteredRecords(filtered);
+      setFilteredRecords(data.records);
     } catch (error) {
       console.log(error);
     }
   };
 
-  const formatDate = (date, isPaymentDate = false) => {
+  const formatDate = (date) => {
     if (!date) {
       return "";
     }
     const d = new Date(date);
-    let month = d.getMonth() + 1;
-    let year = d.getFullYear();
-  
-    if (isPaymentDate) {
-      month += 1;
-      if (month > 12) {
-        month = 1; // reset to January
-        year++; // increment year
-      }
-    }
-  
     return isNaN(d.getTime())
       ? ""
-      : `${d.getDate().toString().padStart(2, "0")}/${month
+      : `${d.getDate().toString().padStart(2, "0")}/${(d.getMonth() + 1)
           .toString()
-          .padStart(2, "0")}/${year}`;
+          .padStart(2, "0")}/${d.getFullYear()}`;
   };
 
-  
   const sortRecords = (records) => {
     return records.sort((a, b) => {
       const dateA = new Date(a[sortConfig.key]);
@@ -181,11 +114,11 @@ const Tableau = () => {
   ];
   
 
-  const filterRecords = (period, status, hasConnectingDate) => {
+  const filterRecords = (period, status) => {
     const now = new Date();
     const oneDay = 24 * 60 * 60 * 1000;
   
-    let filteredByPeriod = records.filter((record) => {
+    const filteredByPeriod = records.filter((record) => {
       const recordDate = new Date(record.CreatedDate);
       const diffDays = Math.round(Math.abs((now - recordDate) / oneDay));
   
@@ -201,71 +134,22 @@ const Tableau = () => {
       }
     });
   
-    let filteredByStatus =
-      status === "Tous"
-        ? filteredByPeriod
-        : filteredByPeriod.filter(
-            (record) =>
-              record.Status__c === status || record.ConnectionStatus__c === status
-          );
-  
-    if (hasConnectingDate) {
-      filteredByStatus = filteredByStatus.filter((record) => {
-        if (
-          record.ConnectingDatePlanned__c &&
-          record.ConnectingDatePlanned__c.length > 0
-        ) {
-          const connectingDate = new Date(record.ConnectingDatePlanned__c);
-          const diffDaysConnecting = Math.round(
-            Math.abs((now - connectingDate) / oneDay)
-          );
-          return diffDaysConnecting >= -1 && diffDaysConnecting <= 1;
-        }
-        return false;
-      });
-    }
-  
-    if (selectedMonth) {
-      filteredByStatus = filteredByStatus.filter((record) => {
-        if (record.DateOfPayment__c) {
-          const paymentMonth = new Date(record.DateOfPayment__c).getMonth() + 1;
-          return parseInt(selectedMonth) === paymentMonth;
-        }
-        return false;
-      });
-    }
+    const filteredByStatus =
+    status === "Tous"
+      ? filteredByPeriod
+      : filteredByPeriod.filter((record) => record.Status__c === status || record.ConnectionStatus__c === status); // Modifiez cette ligne pour inclure le statut EnCoursDeRattrapage
 
-    if (selectedDate) {
-      const selectedMonth = selectedDate.getMonth();
-      const selectedYear = selectedDate.getFullYear();
-      
-      filteredByStatus = filteredByStatus.filter((record) => {
-        if (record.DateOfPayment__c) {
-          const paymentDate = new Date(record.DateOfPayment__c);
-          const paymentMonth = paymentDate.getMonth();
-          const paymentYear = paymentDate.getFullYear();
-      
-          // Check if payment year and month matches the selected year and month - 1
-          const isSameYear = selectedYear === paymentYear;
-          const isPreviousMonth = (selectedMonth === 0 && paymentMonth === 11 && !isSameYear) 
-                                || (selectedMonth === paymentMonth + 1 && isSameYear);
-    
-          return isPreviousMonth;
-        }
-        return false;
-      });
-    }
-    
-  
     return filteredByStatus;
   };
-  
 
-  const handleFilter = (period, status, hasConnectingDate) => {
-    const filtered = filterRecords(period, status, hasConnectingDate);
+
+  const handleFilter = (period, status) => {
+    const filtered = filterRecords(period, status);
     setFilteredRecords(filtered);
     setFilter({ period, status });
   };
+  
+  
 
   useEffect(() => {
   fetchData();
@@ -279,15 +163,11 @@ const Tableau = () => {
   
   const pageCount = Math.ceil(filteredRecords.length / PER_PAGE);
   
-// ...
-const sortedRecords = sortRecords(filteredRecords).map((record) => ({
+  const sortedRecords = sortRecords(filteredRecords).map((record) => ({
   ...record,
   CreatedDate: formatDate(record.CreatedDate),
   ConnectingDatePlanned__c: formatDate(record.ConnectingDatePlanned__c),
-  DateOfPayment__c: formatDate(record.DateOfPayment__c, true), // ici on passe true pour ajouter un mois
-}));
-// ...
-
+  }));
 
   const bgColor = useColorModeValue("white", "gray.700");
 
@@ -332,14 +212,8 @@ const sortedRecords = sortRecords(filteredRecords).map((record) => ({
     >
   
 
-      <div style={{ marginTop: "20px" }}></div>
+      <div style={{ marginTop: "60px" }}></div>
       <Flex direction={{ base: "column", md: "column" }} w="100%" alignItems={{ base: 'left', md: 'left' }}>
-      <Input 
-        placeholder="Recherche..."
-        value={searchValue}
-        onChange={handleSearchChange}
-        mb={4}
-      />
       <Link to="/admin/statistiques">
        <Button
         leftIcon={<MdBarChart />}
@@ -349,7 +223,6 @@ const sortedRecords = sortRecords(filteredRecords).map((record) => ({
        >
       Statistiques
       </Button>
-
   </Link>
   <Box mb={4}>
 
@@ -392,26 +265,11 @@ const sortedRecords = sortRecords(filteredRecords).map((record) => ({
       onClick={() => handleFilter(filter.period, status)}
       px={10}
     >
-       {t(status)}
+      {status}
     </Button>
-    
-    
   ))}
-
 </ButtonGroup>
 
-  <Box mb={4}
-  spacing={20}
-  >
-     <p>Facturation :</p>
-  <DatePicker
-  selected={selectedDate}  // La date actuellement sélectionnée
-  onChange={handleDateChange}  // Le gestionnaire pour changer la date
-  dateFormat="MM/yyyy"  // Le format de la date
-  showMonthYearPicker  // Pour afficher le sélecteur de mois/année
-  placeholderText="Choisir une date"  // Le texte affiché lorsque rien n'est sélectionné
-/>
-  </Box>
     </Box>
 
 </Flex>
@@ -420,7 +278,6 @@ const sortedRecords = sortRecords(filteredRecords).map((record) => ({
 <Table variant="simple"
 overflow={{ base: "auto", md: "auto" }}>
   <Thead>
-    
     <Tr>
       <Th>Détails</Th>
       <Th
@@ -430,13 +287,7 @@ overflow={{ base: "auto", md: "auto" }}>
         Date de la vente
       </Th>
       <Th>Nom</Th>
-      <Th>Numéro de téléphone</Th>
-      <Th
-  onClick={() => toggleSortDirection("ConnectingDatePlanned__c")}
-  style={{ cursor: "pointer" }}
->
-  Date de raccordement prévue
-</Th>
+      <Th>Date de raccordement</Th>
       <Th>Statut</Th>
     </Tr>
   </Thead>
@@ -454,15 +305,8 @@ overflow={{ base: "auto", md: "auto" }}>
             </Td>
             <Td>{record.CreatedDate}</Td>
             <Td>{record.TchProspectName__c}</Td>
-                <Td>
-                <a
-                href={`tel:${record.ProspectMobilePhone__c}`}
-                style={{ color: "blue" }}>
-                   {record.ProspectMobilePhone__c}
-                </a>
-                </Td>
             <Td>{record.ConnectingDatePlanned__c}</Td>
-            <Td>{t(record.Status__c)}</Td>
+            <Td>{record.Status__c}</Td>
 
           </Tr>
           <Collapse in={collapsedRowId === record.Id}>
@@ -470,6 +314,15 @@ overflow={{ base: "auto", md: "auto" }}>
               <VStack align="start" mt={2} mb={2}>
                 <Text>
                   <strong>Adresse :</strong> {record.TchAddress__c}
+                </Text>
+                <Text>
+                  <strong>Mobile :</strong>{" "}
+                  <a
+  href={`tel:${record.ProspectMobilePhone__c}`}
+  style={{ color: "blue" }}
+>
+  {record.ProspectMobilePhone__c}
+</a>
                 </Text>
                 <Text>
                   <strong>Statut du raccordement :</strong>{record.ConnectionStatus__c}
@@ -502,7 +355,7 @@ overflow={{ base: "auto", md: "auto" }}>
                   {record.BasketNumber__c}
                 </Text>
                 <Text>
-                  <strong>Commentaire du technicien :</strong>{" "}
+                  <strong>Commentaire du call :</strong>{" "}
                   {record.Comment__c}
                 </Text>
               </VStack>
