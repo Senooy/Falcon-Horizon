e7532e74178a5158eb787fe446933cedfd72af5f
Dokgo <nskhelifi@gmail.com>
2023-06-08 15:45:24 +0200

    menu racco

--- src/routes.js
+++ src/routes.js
@@ -7,11 +7,13 @@ import {
   MdHome,
   MdLock,
   MdOutlineShoppingCart,
+  MdPower,
 } from 'react-icons/md';
 
 // Admin Imports
 import MainDashboard from 'views/admin/default';
 import Statistiques from 'views/admin/statistiques';
+import Tableau from './views/admin/dataTables/components/racco';
 
 // Auth Imports
 import SignInCentered from 'views/auth/signIn';
@@ -31,6 +33,20 @@ const routes = [
     path: '/statistiques',
     icon: <Icon as={MdBarChart} width="20px" height="20px" color="inherit" />,
     component: Statistiques,
+  },
+  {
+    name: 'Mes raccordements',
+    layout: '/admin',
+    path: '/racco',
+    icon: <Icon as={MdPower} width="20px" height="20px" color="inherit" />,
+    component: Tableau,
+  },
+  {
+    name: 'Sign Up',
+    layout: '/auth',
+    path: '/sign-up',
+    icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
+    component: SignUpCentered,
   },
 
 ];
