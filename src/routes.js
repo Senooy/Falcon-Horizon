ff4d55c832a22ebd9fcad4874b3ade4a6856674a
Dokgo <nskhelifi@gmail.com>
2023-06-08 15:39:42 +0200

    menu

--- src/routes.js
+++ src/routes.js
@@ -32,20 +32,6 @@ const routes = [
     icon: <Icon as={MdBarChart} width="20px" height="20px" color="inherit" />,
     component: Statistiques,
   },
-  {
-    name: 'Sign In',
-    layout: '/auth',
-    path: '/sign-in',
-    icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
-    component: SignInCentered,
-  },
-  {
-    name: 'Sign Up',
-    layout: '/auth',
-    path: '/sign-up',
-    icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
-    component: SignUpCentered,
-  },
 
 ];
 
